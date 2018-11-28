d576e7601416448c5ea162d20ad84306519c287c
Rudzani Magoro <magororj@gmail.com>
2018-11-28 10:14:34 -0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,8 @@
+import { NativeGeocoder, NativeGeocoderForwardResult } from '@ionic-native/native-geocoder';
+//
+// import { AngularFireModule } from 'angularfire2';
+// import { AngularFirestoreModule} from 'angularfire2/firestore';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -5,6 +10,23 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { HomePage} from "../pages/home/home";
 import { MyApp } from './app.component';
+import {Geolocation} from '@ionic-native/geolocation';
+// import { MarkersProvider } from '../providers/markers/markers';
+// import { AngularFireModule } from '@angular/fire';
+// import {AngularFirestoreModule } from 'angularfire2/firestore';
+
+
+
+// var config = {
+//   apiKey: "AIzaSyCKzt1uWneqkIobS7gE-Md7GfngdSCgweQ",
+//     authDomain: "traffic-c0d4b.firebaseapp.com",
+//     databaseURL: "https://traffic-c0d4b.firebaseio.com",
+//     projectId: "traffic-c0d4b",
+//     storageBucket: "traffic-c0d4b.appspot.com",
+//     messagingSenderId: "34629877709"
+
+
+// }
 
 @NgModule({
   declarations: [
@@ -13,6 +35,8 @@ import { MyApp } from './app.component';
   ],
   imports: [
     BrowserModule,
+    // AngularFireModule.initializeApp(config),
+    // AngularFirestoreModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -22,8 +46,10 @@ import { MyApp } from './app.component';
   ],
   providers: [
     StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    SplashScreen,NativeGeocoder,
+    Geolocation,
+    {provide: ErrorHandler ,useClass: IonicErrorHandler},
+    // MarkersProvider
   ]
 })
 export class AppModule {}
--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,5 +1,14 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+// import { MarkersProvider } from './../../providers/markers/markers';
+import { Geolocation } from '@ionic-native/geolocation';
+import { Component,ViewChild, ElementRef} from '@angular/core';
+import { IonicPage, NavController, NavParams, AlertController, Option } from 'ionic-angular';
+import leaflet from 'leaflet';
+import {NativeGeocoder,NativeGeocoderForwardResult,} from '@ionic-native/native-geocoder';
+// import mapboxgl from 'mapbox-gl';
+import  "leaflet";
+// import leaflet from 'leaflet';
+import "leaflet-tilelayer-mbtiles-ts";
+
 
 /**
  * Generated class for the MapPage page.
@@ -8,18 +17,108 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
+
+// mapboxgl.accessToken = 'pk.eyJ1IjoicnVkemFuaSIsImEiOiJjam95NTR1ZTEyODQwM3BwYWczMDdybXIwIn0.CmtsDkJ5-s4jrw36CWCy0A';
+// const map = new mapboxgl.Map({
+//     container: 'map',
+//     style: 'mapbox://styles/mapbox/streets-v9'
+// });
+
+declare var L:any;
 @IonicPage()
 @Component({
   selector: 'page-map',
   templateUrl: 'map.html',
 })
 export class MapPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  geoCodeandAdd(arg0: any): any {
+    throw new Error("Method not implemented.");
   }
+  @ViewChild("map") mapContainer: ElementRef;
+  map: any;
 
+  lat:any;
+  lng:any;
+  city:string;
+
+  constructor(
+    private nativeGeocoder: NativeGeocoder,
+    public geo: Geolocation,public navCtrl: NavController,
+     public navParams: NavParams,private alertCtrl: AlertController) {
+  }
   ionViewDidLoad() {
-    console.log('ionViewDidLoad MapPage');
+     console.log('ionViewDidLoad MapPage');
+       
+      this.geo.getCurrentPosition().then(pos => {
+        
+         this.lat = pos.coords.latitude;
+        this.lng =pos.coords.longitude;
+        
+        
+  
+      }).catch( err => console.log(err));
+  
+      
+     }
+  ionViewDidEnter(){
+    this.prepareMap();
+
   }
+  prepareMap(){
+    this.map = leaflet.map("map").fitWorld();
+    leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      
+      
+     maxZoom: 80
+    }).addTo(this.map);
+    this.map.locate({
+     setView:true,
+     
+    maxZoom:10
+  }).on('locationfound', (e) => {
+       console.log('Your location has been found');
+      let markerGroup =leaflet.featureGroup();
+      let marker: any = leaflet.marker([e.latitude, e.longitude]).on('click',() => {
+        // alert('Marker clicked')
+
+        let prompt = this.alertCtrl.create({
+                title: 'Add Infomation',
+               message: "Enter location",
+                inputs: [
+                  {
+                     name: 'info',
+                     placeholder: 'Info'
+                   },
+                ],
+                buttons: [
+                  {
+                    text: 'Cancel',
+                     handler: data => {
+                      console.log('Cancel clicked');
+                   }
+                   },
+                   {
+                     text: 'Save',
+                     handler: data => {
+                      
+                       this.geoCodeandAdd(data.city);
+                    }
+                   }
+                 ]
+             });
+               prompt.present();
+             
+        
+
+
+      })
+      markerGroup.addLayer(marker);
+       this.map.addLayer(markerGroup);
+        })  
+  }
+  
 
 }
+
+ 
+
