f3a254284587790d41ff0ce3f84351176af730c7
Mahlangu1993 <212281972@tut4life.ac.za>
2018-11-30 09:44:50 +0200

    commit

--- src/pages/form/form.ts
+++ src/pages/form/form.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
-
-
+import * as moment from 'moment';
+import { Geolocation } from '@ionic-native/geolocation';
+declare var firebase;
 @IonicPage()
 @Component({
   selector: 'page-form',
@@ -16,17 +17,29 @@ export class FormPage {
   gender;
   ethnic_group;
   location;
-  date;
+  date ;
   time;
-
   people = [];
-
+  lat;
+  lng;
   
-  constructor(public alertCtrl: AlertController, public navCtrl: NavController, public navParams: NavParams) {    
+  constructor(public alertCtrl: AlertController, public navCtrl: NavController, public navParams: NavParams,public geo: Geolocation) {    
+    this.time = moment().format('H:mm a')
+    this.date = moment().format('MMMM DD YYYY');  
+    this.geo.getCurrentPosition().then(pos => {
+      this.lat = pos.coords.latitude;
+    this.lng =pos.coords.longitude;
+        
+  }).catch( err => console.log(err));
 
-  }
+  firebase.database().ref().on("value",(snapshot) =>{
+    snapshot.forEach(element => { 
+     });
+  }) 
+}
 
   save(){
+    firebase.database().ref().push({Gender:this.gender,Race:this.ethnic_group,Age:this.age_group,Occupation:this.occupation,Location:{lat:this.lat,lng:this.lng}})
     this.saveToArray();
     this.showConfirm();
   }
